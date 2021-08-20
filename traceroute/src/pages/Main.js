b8f017861b0680121af1eb6ad8e447c68926dfe9
Nitro <nitro0@naver.com>
2021-08-20 13:49:48 +0900

    Add Polyline Function and change styles

--- traceroute/src/pages/Main.js
+++ traceroute/src/pages/Main.js
@@ -1,40 +1,69 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { compose } from 'redux'
-import * as actions from '../reducers/actions'
+import React from "react";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import * as actions from "../reducers/actions";
 
-import MapTheme from '../assets/MapTheme.json'
-import GoogleMap from 'google-map-react'
+import MapTheme from "../assets/MapTheme.json";
+import GoogleMap from "google-map-react";
 
-import Marker from '../components/Marker'
+import Marker from "../components/Marker";
 
 class Main extends React.Component {
-    render() {
-      console.log(this.props.mapData.GoogleMapKey)
-        return(
-            <>
-                <div className='map'>
-                <GoogleMap
-                    bootstrapURLKeys={{ key: this.props.mapData.GoogleMapKey }}
-                    defaultCenter={this.props.mapData.center}
-                    defaultZoom={this.props.mapData.zoom}
-                    options={{ styles: MapTheme }}
-                    >
-                    
-                    <Marker lat={this.props.route[0]['lat']} lng={this.props.route[0]['lng']} />
-                    <Marker lat={this.props.route[1]['lat']} lng={this.props.route[1]['lng']} />
-                    <Marker lat={this.props.route[2]['lat']} lng={this.props.route[2]['lng']} />
+  // renderPolylines Code From: https://github.com/Dooffy/google-map-react-polyline-example/blob/master/examples/basic/src/components/Map.js
+  renderPolylines(map, maps) {
+    let nonGeodesicPolyline = new maps.Polyline({
+      path: this.props.route,
+      geodesic: false,
+      strokeColor: "#5F7CD3",
+      strokeOpacity: 0.8,
+      strokeWeight: 4,
+    });
+    nonGeodesicPolyline.setMap(map);
 
-                </GoogleMap>
-                </div>
-            </>
-        )
+    var bounds = new maps.LatLngBounds();
+    for (let marker of this.props.route) {
+      bounds.extend(new maps.LatLng(marker.lat, marker.lng));
     }
+    map.fitBounds(bounds);
+  }
+  
+  render() {
+    console.log(this.props.mapData.GoogleMapKey);
+    return (
+      <>
+        <div className="map">
+          <GoogleMap
+            bootstrapURLKeys={{ key: this.props.mapData.GoogleMapKey }}
+            defaultCenter={this.props.mapData.center}
+            defaultZoom={this.props.mapData.zoom}
+            options={{ styles: MapTheme }}
+            yesIWantToUseGoogleMapApiInternals={true}
+            onGoogleApiLoaded={({ map, maps }) =>
+              this.renderPolylines(map, maps)
+            }
+          >
+            <Marker
+              lat={this.props.route[0]["lat"]}
+              lng={this.props.route[0]["lng"]}
+            />
+            <Marker
+              lat={this.props.route[1]["lat"]}
+              lng={this.props.route[1]["lng"]}
+            />
+            <Marker
+              lat={this.props.route[2]["lat"]}
+              lng={this.props.route[2]["lng"]}
+            />
+          </GoogleMap>
+        </div>
+      </>
+    );
+  }
 }
 
 const mapStateToProps = (state) => ({
-    mapData: state.mapData,
-    target: state.target,
-    route: state.route
-})
-export default compose(connect(mapStateToProps, actions))(Main)
+  mapData: state.mapData,
+  target: state.target,
+  route: state.route,
+});
+export default compose(connect(mapStateToProps, actions))(Main);
