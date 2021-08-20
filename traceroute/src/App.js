5f156348ec738edaf2a2c97267d491c77c3dc951
Nitro <nitro0@naver.com>
2021-08-21 01:28:53 +0900

    Testing react-mapbox-gl library

--- traceroute/src/App.js
+++ traceroute/src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import ReactMapboxGl, { ZoomControl, Marker, Layer } from "react-mapbox-gl";
+
+const { token, styles } = require("./config.json");
+
+const Map = ReactMapboxGl({ accessToken: token });
+
+const mapStyle = {
+  flex: 1,
+};
+
+var paintLayer = {
+  "fill-extrusion-color": "#141414",
+  "fill-extrusion-height": {
+    type: "identity",
+    property: "height",
+  },
+  "fill-extrusion-base": {
+    type: "identity",
+    property: "min_height",
+  },
+  "fill-extrusion-opacity": 0.6,
+};
 
 function App() {
+  const state = {
+    map: null
+  };
+
+  const onStyleLoad = (map)=>{
+    this.setState({map})
+    // this.props.finishedLoading()
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Map
+      className="Map"
+      style={styles.dark}
+      containerStyle={mapStyle}
+      zoom={[15]}
+      center={[-0.0824952, 51.5144951]}
+      pitch={[60]}
+      bearing={[-60]}
+      onStyleLoad={onStyleLoad}
+      mapOptions={{
+        renderWorldCopies: false,
+      }}
+    >
+      <Marker coordinates={[-0.0826952, 51.6154951]}>
+        <div className="Marker"/>
+      </Marker>
+      <Layer
+        id="3d-buildings"
+        sourceId="composite"
+        sourceLayer="building"
+        filter={["==", "extrude", "true"]}
+        type="fill-extrusion"
+        minZoom={14}
+        paint={paintLayer}
+      />
+      <ZoomControl />
+    </Map>
   );
 }
 
